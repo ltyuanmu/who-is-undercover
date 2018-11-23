4ad724b11994dc0ca35a74c0a4a11f779068e357
liangteng1 <teng.liang@newtouch.cn>
2018-11-23 17:51:35 +0800

    first commit

---
+++ backend/src/main/java/com/lt/whoisundercover/WhoIsUndercoverApplication.java
@@ -0,0 +1,12 @@
+package com.lt.whoisundercover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoIsUndercoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoIsUndercoverApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/lt/whoisundercover/WhoIsUndercoverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lt.whoisundercover;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhoIsUndercoverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
